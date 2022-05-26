c33c9e5be03bc396aec9aced73a2c0c615af1bd8
Noman Summar <raja.noman.summar@gmail.com>
2022-05-26 05:34:10 +0500

    fix

--- insideev.py
+++ insideev.py
@@ -5,14 +5,16 @@ from scrapy import Spider, Request
 class InsideEv(Spider):
     name = 'InsideEv'
     base_url = 'https://insideevs.com'
-    start_urls = ['https://insideevs.com/news/?p=1']
+    start_urls = ['https://insideevs.com/news/?p=1',
+                  'https://cleantechnica.com/category/clean-transport-2/electric-vehicles/'
+                  ]
 
     custom_settings = {
         'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/101.0.4951.67 Mobile Safari/537.36',
         'FEED_EXPORT_ENCODING': 'UTF-8',
         'FEEDS': {
-            f"{name}.xlsx": {"format": "xlsx"}
+            f"posts.xlsx": {"format": "xlsx"}
         },
         "FEED_EXPORTERS": {
             'xlsx': 'scrapy_xlsx.XlsxItemExporter',
@@ -20,34 +22,72 @@ class InsideEv(Spider):
     }
 
     def parse(self, response, **kwargs):
-        if '<h1>404 Page not found</h1>' in response.text:
-            return
-        links = response.css('.browseBox-half h3 a::attr(href)').extract()
-        for link in links:
-            yield Request(
-                url=self.base_url + link,
-                callback=self.get_article
-            )
-
+        if 'cleantech' in response.url:
+            articles = response.css('.zox-art-title a::attr(href)').extract()
+            for link in articles:
+                yield Request(
+                    url=link,
+                    callback=self.get_article
+                )
+                # 1775
+            if "exist" not in response.meta:
+                last_page = response.css('.pagination span::text').get()
+                if last_page:
+                    try:
+                        last_page = int(last_page.split('of')[-1].strip())
+                    except:
+                        last_page = 1775
+                else:
+                    last_page = 1775
+                for page in range(2, last_page + 1):
+                    url = 'https://cleantechnica.com/category/clean-transport-2/electric-vehicles/page/{}/'.format(page)
+                    yield Request(
+                        url=url,
+                        callback=self.parse,
+                        meta={"exist": True}
+                    )
         else:
-            url = 'https://insideevs.com/news/?p={}'
-            next_page = int(response.request.url.split('=')[-1].strip())
-            yield Request(
-                url=url.format(str(next_page + 1)),
-                callback=self.parse
-            )
+            if '<h1>404 Page not found</h1>' in response.text:
+                return
+            links = response.css('.browseBox-half h3 a::attr(href)').extract()
+            for link in links:
+                yield Request(
+                    url=self.base_url + link,
+                    callback=self.get_article
+                )
+
+            else:
+                url = 'https://insideevs.com/news/?p={}'
+                next_page = int(response.request.url.split('=')[-1].strip())
+                yield Request(
+                    url=url.format(str(next_page + 1)),
+                    callback=self.parse
+                )
 
     def get_article(self, response):
-        heading = response.css('.m1-article-title::text').get()
-        some_text = response.css('p::text').get()
-        link = response.url
-        date = response.css('.date-data::text').get()
-        yield {
-            'Date': date,
-            "Headline": heading,
-            'Some text': some_text,
-            'URL': link
-        }
+        if 'insideevs' in response.url:
+            heading = response.css('.m1-article-title::text').get()
+            some_text = response.css('p::text').get()
+            link = response.url
+            date = response.css('.date-data::text').get()
+            yield {
+                'Date': date,
+                "Headline": heading,
+                'Some text': some_text,
+                'URL': link
+            }
+        else:
+            url = response.url
+            some_text = response.css('.zox-post-body p::text').get()
+            if not some_text or len(some_text) < 150:
+                some_text = response.css('p:nth-child(2)::text').get()
+
+            yield {
+                'Date': response.css('.post-date::attr(datetime)').get(),
+                "Headline": response.css('.entry-title::text').get(),
+                'Some text': some_text,
+                'URL': url
+            }
 
 
 if __name__ == '__main__':
